c7d86fc495cb0fa1df933743a34e429bde82c833
Mass <massbatht@gmail.com>
2026-03-03 15:24:58 +0000

    add service-account

---
+++ service-account/app.py
@@ -0,0 +1,18 @@
+from flask import flask , jsonify
+
+app = Flask(__name__)
+
+accounts =  {
+        "1": {"balance": 1000},
+        "2": {"balance": 500} }
+
+@app.route("/account/<user_id>")
+
+def account(user_id):
+    if user_id in accounts:
+        return jsonify(accounts[user_id])
+    return jsonify({"error": "User not found"}), 404
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
+
