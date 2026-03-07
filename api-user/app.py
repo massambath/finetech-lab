8ed29b14e6aa128d3e0da882e75409cae200007f
Mass <massbatht@gmail.com>
2026-03-07 06:13:48 +0000

    last updates

--- api-user/app.py
+++ api-user/app.py
@@ -19,6 +19,6 @@ def transfer():
     except:
         return jsonify({"error":"Payment service unreachable"}), 500
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
